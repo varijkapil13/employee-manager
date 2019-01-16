5d1e2ec37d0e9ea2491400511fa34e98bb79052f
Varij Kapil <varijkapil13@live.com>
2019-01-16 15:12:41 +0530

    fixed prettier

--- server/routes/workday.js
+++ server/routes/workday.js
@@ -3,21 +3,18 @@ import multer from 'multer';
 import * as XLSX from 'xlsx';
 
 const routes = express.Router();
-const upload = multer({ storage: multer.memoryStorage() });
+const upload = multer({storage: multer.memoryStorage()});
 routes.post('/upload', upload.single('reportFile'), (req, res, next) => {
+  const workbook = XLSX.read(req.file.buffer, {type: 'buffer'});
+  const sheet_name_list = workbook.SheetNames;
+  const sheet_in_json = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[1]]);
+  for (let element of sheet_in_json) {
+    console.log(element);
+  }
 
-    const workbook = XLSX.read(req.file.buffer, {type: "buffer"});
-    const sheet_name_list = workbook.SheetNames;
-    const sheet_in_json = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[1]]);
-     for (let element of sheet_in_json){
-         console.log(element);
-     }
-
-
-    return res.status(200).send({
-        status: true,
-    });
+  return res.status(200).send({
+    status: true
+  });
 });
-
 
 export default routes;
