0bc7036aa3d53deb4dadb2d3983a2ecf9a631b82
Varij Kapil <varijkapil13@live.com>
2019-01-16 22:22:36 +0530

    feat: added holiday addition, deletion and get api

---
+++ server/controllers/holiday.js
@@ -0,0 +1,60 @@
+import model from '../models';
+
+const {Holiday} = model;
+
+class HolidayController {
+  static addHoliday(req, res) {
+    const {date, notes, from, to} = req.body;
+
+    return Holiday.create({
+      date,
+      notes,
+      from,
+      to
+    })
+      .then(holidayData =>
+        res.status(201).send({
+          success: true,
+          message: 'Holiday was successfully added',
+          holidayData
+        })
+      )
+      .catch(error => res.status(400).send(error));
+  }
+
+  static deleteHoliday(req, res) {
+    const holidayId = req.params.holidayId;
+
+    Holiday.findById(holidayId).then(holiday => {
+      if (!holiday) {
+        return res.status(400).send({
+          message: 'Avatar Not Found'
+        });
+      } else {
+        holiday
+          .destroy()
+          .then(() =>
+            res.status(200).send({
+              message: 'Avatar successfully deleted'
+            })
+          )
+          .catch(error => res.status(400).send(error));
+      }
+    });
+  }
+
+  static getAllHolidays(req, res) {
+    Holiday.findAll().then(holidays => {
+      return res
+        .status(200)
+        .send({
+          status: true,
+          message: 'Found holidays',
+          holidays
+        })
+        .catch(error => res.status(400).send(error));
+    });
+  }
+}
+
+export default HolidayController;
--- server/routes/holiday.js
+++ server/routes/holiday.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import HolidayController from '../controllers/holiday';
 
 const routes = express.Router();
+
+routes.post('/', HolidayController.addHoliday);
+routes.delete('/', HolidayController.deleteHoliday);
+routes.get('/', HolidayController.getAllHolidays);
 
 export default routes;
